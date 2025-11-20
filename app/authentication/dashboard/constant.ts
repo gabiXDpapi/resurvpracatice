e0390c1858b9d32e104dccb7eb6946d5de80daed
Gabriel Mosquera <mosqueragabriel99@gmail.com>
2025-11-20 23:23:47 +0800

    Dashboard Props
    Turned the Facility Cards into props for modularity

---
+++ app/authentication/dashboard/constant.ts
@@ -0,0 +1,17 @@
+export const facilities = [
+  {
+    title: "Convention Center",
+    imageSrc: "/Convention.jpg",
+    href: "/authentication/dashboard",
+  },
+  {
+    title: "Center for Continuing Education",
+    imageSrc: "/CCE.jpg",
+    href: "/authentication/dashboard",
+  },
+  {
+    title: "Beach Garden",
+    imageSrc: "/Beach_Resort.jpg",
+    href: "/eventspaces/beachgarden",
+  },
+];
