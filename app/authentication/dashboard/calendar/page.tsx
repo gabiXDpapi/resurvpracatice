f73493a60403eda17a833182d5852d8708ff220f
Gabriel Mosquera <mosqueragabriel99@gmail.com>
2025-12-03 17:03:30 +0800

    Added Events List
    Allow Users to see what events take place in specific dates in their respective event space

--- app/authentication/dashboard/calendar/page.tsx
+++ app/authentication/dashboard/calendar/page.tsx
@@ -1,43 +1,21 @@
-"use client"
 import * as React from "react"
 import { AppSidebar } from "@/components/app-sidebar"
-import { Button } from "@/components/ui/button";
-import Image from "next/image"
-import { Calendar } from "@/components/ui/calendar" 
-import { facilities } from '@/app/authentication/dashboard/constant';
-import { notFound } from "next/navigation"
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator, 
-} from "@/components/ui/breadcrumb"
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Separator } from "@/components/ui/separator"
+import { EventCalendarCard, CalendarEvent } from "@/components/EventCalendarCard"
+import { getEvents } from "../../../../lib/eventsdata"
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
+import { Separator } from "@/components/ui/separator"
 import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
 
 interface PageProps {
   params: Promise<{
@@ -45,85 +23,38 @@ interface PageProps {
   }>;
 }
 
-export default function CalendarPage({ params }: PageProps){
-  const [date, setDate] = React.useState <Date | undefined>(
-    new Date(2025, 5,12)
-  )
-  const bookedDates = Array.from(
-    { length: 5 },
-    (_, i) => new Date(2025, 11, 1 + i)
-  )
-  
+export default async function CalendarPage({ params }: PageProps) {
+  const events = await getEvents();
   return (
     <SidebarProvider>
-      <AppSidebar/>
+      <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 bg-[#CEDBEE]">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-            <Breadcrumb >
+            <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+            <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="/authentication/dashboard" className= "text-black">
+                  <BreadcrumbLink href="/authentication/dashboard" className="text-black">
                     Dashboard
                   </BreadcrumbLink>
                 </BreadcrumbItem>
                 <BreadcrumbSeparator className="hidden md:block" />
                 <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="/authentication/dashboard" className= "text-black">
+                  <BreadcrumbLink href="/authentication/dashboard" className="text-black">
                     Calendar
                   </BreadcrumbLink>
                 </BreadcrumbItem>
               </BreadcrumbList>
             </Breadcrumb>
-       </div>
+          </div>
         </header>
+        
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0 bg-[#EEF4ED]">
-          
-            <div className="flex flex-col w-full max-w-5xl mx-auto mt-2 p-4 gap-6 bg-[#dce5f2] border border-slate-400 rounded-xl shadow-sm overflow-hidden items-stretch justify-center ">
-            <Select>
-            <SelectTrigger className="w-[190px] bg-[#EEF4ED] border border-[#556378]">
-              <SelectValue placeholder="Select an Event Space" />
-              </SelectTrigger>
-              <SelectContent className="bg-[#EEF4ED]" >
-                <SelectGroup>
-                  {facilities.map((facility, index) => (
-                  <SelectItem 
-                    key={index} 
-                    value={facility.title}
-                  >
-                    {facility.title}
-                  </SelectItem>
-                  ))}
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-
-            <Calendar
-                mode="single"
-                selected={date}
-                onSelect={setDate}
-                disabled={bookedDates}
-                modifiers={{
-                booked: bookedDates, 
-                }}
-                modifiersClassNames={{
-                booked: 
-                "bg-red-100 text-red-400 " + 
-                "line-through decoration-red-400 " +
-                "cursor-not-allowed " +
-                "opacity-100 " +
-                "[&>button]:hover:bg-red-100 [&>button]:hover:text-red-400 [&>button]:cursor-not-allowed",
-                }}
-                className="rounded-lg border bg-[#EEF4ED] text-[#556378] p-3 w-full h-full "
-            />
-            </div>
-          
+          <EventCalendarCard events={events} />
         </div>
+        
       </SidebarInset>
     </SidebarProvider>
   )
